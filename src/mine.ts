494f6038b026cbb82a64cf4cc891b9616ed7c0d3
Michael L Perry <michael@qedcode.com>
2018-08-25 20:42:38 -0500

    Test the SHA to see if it meets the target.

--- src/mine.ts
+++ src/mine.ts
@@ -6,9 +6,18 @@ export async function mine(target: number) {
     const repo = simpleGit();
     const status = await repo.status();
     const files = [].concat(status.created, status.staged, status.deleted);
-    const result = await repo.commit('message');
-    console.log(`Created commit ${result.commit} with ${files.length} files.`);
-    await repo.reset(['HEAD~1']);
-    await repo.add(files);
-    console.log('Reset');
+    for (let attempt = 0; attempt < 3; attempt++) {
+        const result = await repo.commit('message');
+        console.log(`Created commit ${result.commit} with ${files.length} files.`);
+        const sha = parseInt(result.commit, 16);
+        const prefix = sha >> (28-target);
+        if (prefix > 0) {
+            console.log('Commit does not match target. Resetting.');
+            await repo.reset(['HEAD~1']);
+            await repo.add(files);
+        }
+        else {
+            break;
+        }
+    }
 }
