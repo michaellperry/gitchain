b8665a31233fcf5840fe93b2140709898706f201
Michael L Perry <michael@qedcode.com>
2018-08-25 20:22:12 -0500

    Reset and re-add the staged files.

--- src/mine.ts
+++ src/mine.ts
@@ -4,6 +4,11 @@ export async function mine(target: number) {
     console.log(`Mining for ${target} ${target === 1 ? 'bit' : 'bits'}.`);
 
     const repo = simpleGit();
+    const status = await repo.status();
+    const staged = status.staged;
     const result = await repo.commit('message');
-    console.log(`Success: ${result}.`);
+    console.log(`Created commit ${result.commit}.`);
+    await repo.reset(['HEAD~1']);
+    repo.add(staged);
+    console.log('Reset');
 }
