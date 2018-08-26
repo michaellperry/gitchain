3d4ffb759bca1a4a37a241172099cf8cc3de784b
Michael L Perry <michael@qedcode.com>
2018-08-25 19:45:30 -0500

    Call simple-git.

--- src/mine.ts
+++ src/mine.ts
@@ -1,3 +1,15 @@
+import { default as simpleGit } from "simple-git";
+
 export function mine(target: number) {
     console.log(`Mining for ${target} ${target === 1 ? 'bit' : 'bits'}.`);
+
+    const repo = simpleGit(__dirname);
+    repo.commit('message', (err: any, result: any) => {
+        if (err) {
+            console.log(`Error: ${err}.`);
+        }
+        else {
+            console.log(`Success: ${result}.`);
+        }
+    });
 }
